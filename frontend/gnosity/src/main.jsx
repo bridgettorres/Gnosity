0531bf4f72250963e0a4abfeab1b60f9e87a8c13
ReRe-Create <liureanna@gmail.com>
2025-11-15 16:25:22 -0500

    Connected the dashboard and login through routing

--- frontend/gnosity/src/main.jsx
+++ frontend/gnosity/src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import LoginSignup from './components/signup.jsx';
+
+const router = createBrowserRouter([
+  {path:"/", element: <App/>},
+  {path:"/login", element: <LoginSignup />},
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
