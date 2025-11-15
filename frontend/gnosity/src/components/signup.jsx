a22bec322580f1bbbb5e40f0290a4fa109eb9039
ReRe-Create <liureanna@gmail.com>
2025-11-15 12:24:08 -0500

    added the login section

--- frontend/gnosity/src/components/signup.jsx
+++ frontend/gnosity/src/components/signup.jsx
@@ -1,27 +1,45 @@
+import React, { useState } from 'react';
 import './style/card.css';
-import './style/signup.css'
+import './style/signup.css';
 
 const LoginSignup = () => {
+
+    const [action, setAction] = useState("Sign Up");
+
     return (
         <div className='card'>
-            <div className="title">Sign Up</div>
+            <div className="title">{action}</div>
             <div className='subtext'>Are you ready to level up irl?</div>
             <div className='inputs'>
                 <div className='input'>
                     <input type="text" placeholder='Username' />
                 </div>
-                <div className='input'>
+
+               
+                {action === "Login" ? <div></div> :
+                    <div className='input'>
                     <input type="email" placeholder='Email' />
                 </div>
+                } 
+
                 <div className='input'>
                     <input type="password" placeholder='Password' />
                 </div>
+                </div>
+
+            {action === "Sign Up" ?<div></div> :
+                <div className="forgot-password">Forgot Password? <span>Click Here!</span>
             </div>
-            <div className="forgot-password">Already have an account? <span>Click Here!</span>
+            }
+
+            {action === "Login" ?<div></div> :
+                <div className="forgot-password">Have an account? <span>Click Here!</span>
             </div>
+}
+
             <div className='submit-container'>
-                <div className="submit">Sign Up</div>
-                <div className="submit">Login</div>
+                <div className={action === "Login"?"submit light":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+                <div className={action === "Sign Up"?"submit light":"submit"}onClick={()=>{setAction("Login")}}>Login</div>
             </div>
         </div>
     )
